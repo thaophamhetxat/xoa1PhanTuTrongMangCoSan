411832e99e2b3f439977025e1f787b0050593b83
thaophamhetxat <thaophamhetxat@gmail.com>
2021-06-08 10:03:33 +0700

    sua

--- src/com/codegym/Main.java
+++ src/com/codegym/Main.java
@@ -6,32 +6,43 @@ public class Main {
 
     public static void main(String[] args) {
         //Khai báo và khởi tạo mảng số nguyên gồm N phần tử cho trước.
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        int[] arr = {1, 2, 3, 0, 5, 6, 7, 8, 9, 0};
         System.out.printf("show arr: ");
         for (int i = 0; i < arr.length; i++) {
             System.out.printf(arr[i] + "\t");
         }
         //Nhập X là phần tử cần xoá
         int number;
+        int position;
+        int newArr = arr.length-1;
         Scanner scanner = new Scanner(System.in);
+
         System.out.printf("\n" + "enter number delete: ");
         number = scanner.nextInt();
-        int newArr = arr.length;
+        System.out.printf("\n" + "enter position delete: ");
+        position = scanner.nextInt();
+
 //Tìm X xem có xuất hiện trong mảng không. Nếu xuất hiện chỉ ra vị trí của X. Giả sử vị trí này là: i=number
         //Thực hiện xoá phần tử X khỏi mảng
         //Duyệt mảng từ vị trí cần xoá (number) đến cuối mảng (arr.length-1)
+        if (position <= 0 || position > 10) {
+            System.out.println("position no in arr");
+        } else {
+            for (int i = position - 1; i < arr.length - 1; i++) {
+                arr[i] = number;
+                arr[i] = arr[i + 1];
 
-        for (int i = number; i < arr.length - 1; i++) {
-            arr[i] = arr[i + 1];
-
+            }
+            newArr--;
         }
-        newArr--;
+
+
         // Gán phần tử 0 ở vị trí cuối newarr cho phần tử ở vị trí newarr + 1
         //Kết thúc duyệt mảng. In ra mảng.
 
-        System.out.printf("new arr: ");
-        for (int i = 0; i < newArr + 1; i++) {
-            System.out.printf(arr[i] + "\t");
+        System.out.print("new arr: ");
+        for (int i = 0; i < newArr +1; i++) {
+            System.out.print(arr[i]);
         }
 
 
